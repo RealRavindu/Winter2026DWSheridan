4a5c18db6d7b56e61bd59c4e82bbd4eca325bd79
James-Sutton-777 <suttjame@sheridancollege.ca>
2026-03-03 16:00:26 -0500

    completed breathing behavior

--- Assets/Jim Stuff/Scripts/BreathingScript.cs
+++ Assets/Jim Stuff/Scripts/BreathingScript.cs
@@ -1,73 +1,92 @@
+using System.Collections;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 
 public class BreathingScript : MonoBehaviour
 {
-    public float oxygenLevelCap = 100f;
-    [SerializeField] float oxygenLevel = 100f;
-    [SerializeField] float oxygenConsumtionRate = 1f;
-    [SerializeField] float oxygenReplenishRate = 1f;
-    [SerializeField] float idealReplenishRange;
-    [SerializeField] float idealReplenishFraction = 3f;
-    [SerializeField] float replenishTimer = 0f;
-    [SerializeField] float replenishTimerLimit = 5f;
-    [SerializeField] GameObject player;
-    [SerializeField] Vector3 point1;
-    [SerializeField] Vector3 point2;
-    [SerializeField] float velocity;
-    [SerializeField] float velocitymodifier;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float firstInput;
+    private float secondInput;
+    private float difference;
+    private float differenceCheck;
+    private float rate;
+    public float time;
+    public float faintThreshold;
+
+    //For check values coroutine
+    private int inputs = 0;
+    public float interval = 1f;
+    private float inputsPerSecond;
+    public float maximum;
+    public float minimum;
+
+    private void Start()
     {
+        //StartCoroutine(CheckValues());
     }
 
     private void Update()
     {
-        if (player != null)
+        time = Time.time;
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            if (point1 == null)
-            {
-                point1 = player.transform.position;
-            }
-            if (point2 == null && point1 != null)
-            {
-                point2 = player.transform.position; 
-            }
-            velocity = point1.magnitude;
-            Debug.Log("Current velocity is: " + velocity);
-            if (oxygenLevel > 0)
-            {
-                float modifier = oxygenConsumtionRate * (velocity * velocitymodifier);
-                oxygenLevel -= modifier * Time.deltaTime;
-                Debug.Log($"Decreased oxygen level by {modifier}, oxygen level now {oxygenLevel}");
-            }
-
-            if (Input.GetKeyDown(KeyCode.M))
-            {
-                if (oxygenLevel < idealReplenishRange && idealReplenishRange == 0)
-                {
-                    replenishTimer = 0f;
-                    idealReplenishRange = oxygenLevel * (1 / idealReplenishFraction);
-                }
-                oxygenLevel += oxygenReplenishRate * Time.deltaTime;
-                Debug.Log($"Increased oxygen level by {oxygenReplenishRate}, oxygen level now {oxygenLevel}, ideal replenish range now {idealReplenishRange}");
-            }
-            if(idealReplenishRange > 0)
-            {
-                replenishTimer += Time.deltaTime;
-                Debug.Log($"replenishTimer = {replenishTimer}");
-            }
-            if (replenishTimer >= replenishTimerLimit)
-            {
-                Debug.Log("Character fainted due to poor oxygen intake");
-            }
+            //inputs++;
+            InputWithInterval();
+        }
+        if (difference <= (Time.time - secondInput))
+        {
+            difference = Time.time - firstInput;
+            Debug.Log($"rate changing difference = {difference}");
+        }
+        rate = 1 / difference;
+        Debug.Log($"{rate}/s");
+        if (rate < 0.5f)
+        {
+            differenceCheck = Time.time - secondInput;
+        }
+        if (differenceCheck > faintThreshold)
+        {
+            Debug.Log("You have passed out!");
         }
     }
 
-    private void LogMessage()
+    private void InputWithInterval()
     {
-        Debug.Log("Current velocity is: " + velocity);
-        Debug.Log($"replenishTimer = {replenishTimer}");
-        Debug.Log($"Increased oxygen level by {oxygenReplenishRate}, oxygen level now {oxygenLevel}, ideal replenish range now {idealReplenishRange}");
-        Debug.Log($"Decreased oxygen level by modifier, oxygen level now {oxygenLevel}");
+        if (firstInput != 0)
+        {
+            firstInput = secondInput;
+            secondInput = Time.time;
+        }
+        else
+        {
+            firstInput = Time.time;
+            secondInput = firstInput;
+            Debug.Log("Initial input");
+        }
+        difference = secondInput - firstInput;
+        Debug.Log($"Input detect difference = {difference}");
+
+
     }
+
+    private IEnumerator CheckValues()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+
+            if (interval == 1)
+            {
+                inputsPerSecond = inputs;
+            }
+            else
+            {
+                float divider = 1 / interval;
+                inputsPerSecond = inputs * divider;
+            }
+            Debug.Log($"{inputs} inputs made during interval");
+            inputs = 0;
+            Debug.Log($"{inputsPerSecond} breadths per second");
+        }
+    }
+
 }
