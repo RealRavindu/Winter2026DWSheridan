933a258f1eff28f7256c89f2643e5d4512efeca8
hewabadd <hewabadd@OA-S139-20>
2026-03-03 14:42:18 -0500

    added oxygen and blood bar to rav scene

---
+++ Assets/Rav Stuff/Scripts/BarScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class BarScript : MonoBehaviour
+{
+    public Slider oxygenBar;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
