9d1e8cf4fb5e96fc0d2229dc31bb439a2b3d3ef6
hewabadd <hewabadd@OA-S139-20>
2026-03-03 15:42:38 -0500

    Implemented a temp obj into the scene that holds the bar script which was another temp script to simulate oxygen rate by presing spacebar.
    First person controller script was modified to include numpad keys to rotate camera.

--- Assets/Rav Stuff/Scripts/BarScript.cs
+++ Assets/Rav Stuff/Scripts/BarScript.cs
@@ -3,11 +3,18 @@ using UnityEngine.UI;
 public class BarScript : MonoBehaviour
 {
     public Slider oxygenBar;
+    [SerializeField] private float oxygenRate, timeOfLastBreath;
+    public float oxygenDecreaseRate;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            Debug.Log("Spacebar pressed");
+            timeOfLastBreath = Time.time;
         }
+
+        oxygenRate = 1 / (Time.time - timeOfLastBreath);
+        Debug.Log(Time.time);
+        oxygenBar.value = oxygenRate;
     }
 }
